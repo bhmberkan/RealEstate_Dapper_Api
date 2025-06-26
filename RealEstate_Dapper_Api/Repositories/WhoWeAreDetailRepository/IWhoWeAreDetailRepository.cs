3af02fa5710e35e7fc3693120af63954f08b671d
bhmberkan <berkanburakturgut@gmail.com>
2025-06-27 00:21:19 +0300

    Ana Sayfadayı Componentlerine ayırdım tamamen, Layoutu Düzenleyip Componentlere atadım. Veri tabanına iki yeni tablo ekledim. Service ve WhoWeAreDetail kısımları için.  WhoWeAre alanı için api tarafındaki tüm işlemleri tek tek yaptım. Controller,Dto,Repository vs. Daha sonra UI tarafında Consume İşlemini gerçekleştirdim. Ardından ilgili alan için veri girişi yaptım. DefaultHomePageProductList partialı için api ve uı tarafında düzenlemeler yaptım.

---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreDetailRepository/IWhoWeAreDetailRepository.cs
@@ -0,0 +1,15 @@
+﻿using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
+{
+    public interface IWhoWeAreDetailRepository
+    {
+        Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync();
+        void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto);
+        void UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto);
+
+        void DeleteWhoWeAreDetail(int id);
+
+        Task<GetByIdWhoWeAreDetailDto> GetWhoWeAreDetail(int id);
+    }
+}
