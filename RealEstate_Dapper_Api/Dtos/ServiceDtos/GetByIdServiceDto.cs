e13a74a9390eb61cb1716eeb7ea08101ed2e5de1
bhmberkan <berkanburakturgut@gmail.com>
2025-07-03 23:54:02 +0300

    Biz kimiz/WhoWeAre alanını yazmaya başladım UI tarafında consume işlemlerini gerçekleştirdim.
    Service yani WhoWeAre'ın alt kısmı için Api tarafını yazdım daha sonra UI tarafında Consume işlemlerini gerçekletirdim. View sayfalarını yazdım.
    
    Sidebar alanında iconları düzenledim.

--- RealEstate_Dapper_Api/Dtos/ServiceDtos/GetByIdServiceDto.cs
+++ RealEstate_Dapper_Api/Dtos/ServiceDtos/GetByIdServiceDto.cs
@@ -2,7 +2,7 @@
 {
     public class GetByIdServiceDto
     {
-        public int SericeID { get; set; }
+        public int ServiceID { get; set; }
         public string ServiceName { get; set; }
         public bool ServiceStatus { get; set; }
     }
