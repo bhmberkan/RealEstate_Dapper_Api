0057e8c58da2cdbb6d6710eb95e8ab406df051a3
bhmberkan <berkanburakturgut@gmail.com>
2025-07-09 23:11:48 +0300

    Admin Dashboardda TodoList Kısmını oluşturdum. Todolist için Tablo oluşturum api tarafında kodlamasını tamamladım. Daha sonra Consume işlemlerini yazdım.
    Add-Client side library seçeneği ile signalR ekledim.
    SignalR Yapılandırmasını yazdım, SignalR Bağlantı işlemlerini tamamladım.
    Hub Üzerindnen SignalR ile bağlantı kurma işlemini tamamladım(Kategori Sayısı)
    Setlnterval komutu ile ; verlilerin anlık olarak değişmesini sağladım.
    Api Tarafında program.cs te httpclient ve SignalR yapılandırmalarımı yazdım. (addcors,usecors,MapHub)(Apideki program.cs)
    
    JWT işlemlerine başladım.(Tools)
    Generator, JwtTokenDefaults,TokenResponseviewmodel(Dtoda olabilir),GetCheckAppUserViewModel sınıflarımı yazdım.
    
    Estate Agent/Emlakçı için ayrı bir arayüz olacağı için bir Area oluşturdum.
    Daha sonra layoutu geçtim. ilgili controllerlara hangi areada çalıştığımızı ve program.cs'e  UseEndpoints(Area Eklenince eklenmesi gereken) yapısını geçtim.

---
+++ RealEstate_Dapper_Api/Controllers/ToDoListsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ToDoLitRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoListsController : ControllerBase
+    {
+        private readonly IToDoLitRepository _toDoLitRepository;
+
+        public ToDoListsController(IToDoLitRepository toDoLitRepository)
+        {
+            _toDoLitRepository = toDoLitRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ToDoListList()
+        {
+            var values = await _toDoLitRepository.GetAllToDoListAsync();
+            return Ok(values);
+        }
+    }
+}
